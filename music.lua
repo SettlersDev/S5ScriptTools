8b3de9455a0112fdba4d7b4cc96c989d6f88bb94
b0bby <no@thanks.net>
2015-05-18 14:03:42 +0200

    improved music.lua

--- music.lua
+++ music.lua
@@ -11,6 +11,13 @@ function StartMusic(_name, _path)
   LocalMusic.OwnSong = _name
   LocalMusic.OwnPath = _path or nil
 end
+
+function StopMusic()
+	Stream.Stop()
+	Music.Stop()
+	LocalMusic.SongLength = 0
+end
+
 LocalMusic.OwnSongs = {
   ["ac1.mp3"] = 180 + 57,
   ["intro1.mp3"] = 180 + 14,
@@ -72,10 +79,10 @@ function InitOwnMusic(_path, _musicset)
 end
 
 function LocalMusic_UpdateMusic()
-  if Logic.PlayerGetGameState(1) ~= 1 then
-    Stream.Stop()
-    return
-  end
+	if Logic.PlayerGetGameState(1) ~= 1 then
+		Stream.Stop()
+		return
+	end
 	if LocalMusic.BattlesOnTheMap == 0 then
 		if  LocalMusic.SongLength  > Logic.GetTime() then	 
 			return
@@ -88,42 +95,45 @@ function LocalMusic_UpdateMusic()
 		Weather = "snow"
 	end
 	if not LocalMusic.OwnMusic then
-    local SetToUse
-    if LocalMusic.BattlesOnTheMap == 1 then
-      SetToUse = LocalMusic.SetBattle
-    elseif LocalMusic.BattlesOnTheMap == 2 then
-      SetToUse = LocalMusic.SetEvilBattle
-    elseif LocalMusic.BattlesOnTheMap == 0 then
-      SetToUse = LocalMusic.UseSet[Weather]
-    end
-    if (IsBriefingActive ~= nil and IsBriefingActive() == true ) or (IsCutsceneActive~= nil and IsCutsceneActive() == true)then
-      SetToUse = LocalMusic.SetBriefing
-    end
-    local SongAmount = table.getn(SetToUse)
-    local Random = 1 + XGUIEng.GetRandom(SongAmount-1)
-    local path = Folders.Music 
-    local SongToPlay
-    if LocalMusic.UseSet.ownSet then
-      SongToPlay = LocalMusic.OwnSetPath .. SetToUse[Random][1]
-      Stream.Start(SongToPlay, 127)
-    else
-      SongToPlay = path .. SetToUse[Random][1]
-      Sound.StartMusic(SongToPlay, 127)
-    end
-    LocalMusic.SongLength =  Logic.GetTime() + SetToUse[Random][2] + 2
-  else
-    local SongToPlay
-    if LocalMusic.OwnPath then
-      SongToPlay = LocalMusic.OwnPath .. LocalMusic.OwnSong
-      Stream.Start(SongToPlay, 127)
-      Music.Stop()
-    else
-      SongToPlay = Folders.Music .. LocalMusic.OwnSong
-      Stream.Stop()
-      Sound.StartMusic(SongToPlay, 127)
-    end
-    LocalMusic.SongLength =  Logic.GetTime() + LocalMusic.OwnSongs[LocalMusic.OwnSong] + 2
-  end
+		local SetToUse
+		local UseStream
+		UseStream = LocalMusic.UseSet.ownSet
+		if LocalMusic.BattlesOnTheMap == 1 then
+			SetToUse = LocalMusic.SetBattle
+		elseif LocalMusic.BattlesOnTheMap == 2 then
+			SetToUse = LocalMusic.SetEvilBattle
+		elseif LocalMusic.BattlesOnTheMap == 0 then
+			SetToUse = LocalMusic.UseSet[Weather]
+		end
+		if (IsBriefingActive ~= nil and IsBriefingActive() == true ) or (IsCutsceneActive~= nil and IsCutsceneActive() == true)then
+			SetToUse = LocalMusic.SetBriefing
+			UseStream = false
+		end
+		local SongAmount = table.getn(SetToUse)
+		local Random = 1 + XGUIEng.GetRandom(SongAmount-1)
+		local path = Folders.Music 
+		local SongToPlay
+		if UseStream then
+			SongToPlay = LocalMusic.OwnSetPath .. SetToUse[Random][1]
+			Stream.Start(SongToPlay, 127)
+		else
+			SongToPlay = path .. SetToUse[Random][1]
+			Sound.StartMusic(SongToPlay, 127)
+		end
+		LocalMusic.SongLength =  Logic.GetTime() + SetToUse[Random][2] + 2
+	else
+		local SongToPlay
+		if LocalMusic.OwnPath then
+			SongToPlay = LocalMusic.OwnPath .. LocalMusic.OwnSong
+			Stream.Start(SongToPlay, 127)
+			Music.Stop()
+		else
+			SongToPlay = Folders.Music .. LocalMusic.OwnSong
+			Stream.Stop()
+			Sound.StartMusic(SongToPlay, 127)
+		end
+		LocalMusic.SongLength =  Logic.GetTime() + LocalMusic.OwnSongs[LocalMusic.OwnSong] + 2
+	end
 	LocalMusic.BattlesOnTheMap = 0
 	LocalMusic.OwnMusic = false
 end
